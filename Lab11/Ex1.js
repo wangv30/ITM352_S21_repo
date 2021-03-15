37500cfb36dd2e1416517e5a5bdf997d7ead5683
wangv30 <wangv@hawaii.edu>
2021-03-15 13:27:56 -1000

    backup to git

--- Lab11/Ex1.js
+++ Lab11/Ex1.js
@@ -1,1 +1,23 @@
+function isNonNegInt(q, returnErrors = false) {
+    errors = []; // assume no errors at first
+    if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+
+    return returnErrors ? errors : (errors.length == 0);
+}
+
+
 attributes = "Vincent;20;MIS"
+parts = attributes.split(';'); 
+
+for(part of parts) {
+ //   console.log(isNonNegInt(part, true));
+}
+
+function callBack(part) {
+
+}
+parts.forEach(function (item) {
+    console.log( (typeof item == 'string' && item.length > 0)?true:false ) 
+}
