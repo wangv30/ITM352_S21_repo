32a91285f0f09e4d808ff74b6af7987fdda864f2
wangv30 <wangv@hawaii.edu>
2021-05-06 00:49:08 -1000

    backup

---
+++ Testing/public/products.js
@@ -0,0 +1,105 @@
+// Vincent Wang - 3/25/21
+// Creates array of Products
+var hydroflask = [
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},                                                            
+]
+
+var pogo = [
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},                                                            
+]
+
+var fidus = [
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},                                                            
+]
+
+var hydromate = [
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},
+    {
+        "name": "Pogo",
+        "price": 10.00,
+        "image": "pogo.jpg"},                                                            
+]
+
+var products =
+    {
+        "hydroflask": hydroflask,
+        "pogo": pogo,
+        "fidus": fidus, 
+        "hydromate": hydromate
+    }
+    
+if(typeof module != 'undefined') {
+     module.exports.products = products;
+    }
