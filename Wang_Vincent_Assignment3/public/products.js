af59f907a9b719883b1fde2f7d68fd27ddaf94f6
wangv30 <wangv@hawaii.edu>
2021-05-14 00:15:26 -1000

    backup

--- Wang_Vincent_Assignment3/public/products.js
+++ Wang_Vincent_Assignment3/public/products.js
@@ -1,31 +1,95 @@
-// Vincent Wang - 3/25/21
-// Creates array of Products
-products = [
-    {
-    "brand": "Pogo",
-    "price": 10.00,
-    "image": "pogo.jpg"},
+// Vincent Wang
 
+// Creates array of products for hydroflask
+var hydroflask = [
     {
-    "brand": "Fidus",
-    "price": 20.00,
-    "image": "fidus.jpg"},
+        "name": "20 oz Wide Mouth Hydroflask",
+        "price": 38.00,
+        "image": "hydroflask1.jpg"
+    },
+    {
+        "name": "32 oz Wide Mouth Hydroflask",
+        "price": 45.00,
+        "image": "hydroflask2.jpg"
+    },
+    {
+        "name": "40 oz Wide Mouth Hydroflask",
+        "price": 50.00,
+        "image": "hydroflask3.jpg"
+    },
+    {
+        "name": "64 oz Wide Mouth Hydroflask",
+        "price": 65.00,
+        "image": "hydroflask4.jpg"
+    }                                                            
+]
 
+// Creates array of products for pogo
+var pogo = [
     {
-    "brand": "Hydromate",
-    "price": 30.00,
-    "image": "hydromate.jpg"},
+        "name": "12 oz Kids Stainless Steel Pogo",
+        "price": 10.00,
+        "image": "pogo1.png"
+    },
+    {
+        "name": "18 oz Soft Straw Stainless Steel Pogo",
+        "price": 15.00,
+        "image": "pogo2.png"
+    },
+    {
+        "name": "26 oz Soft Straw Stainless Steel Pogo",
+        "price": 20.00,
+        "image": "pogo3.png"
+    }                                                           
+]
 
+// Creates array of products for fidus
+var fidus = [
     {
-    "brand": "Hydroflask",
-    "price": 40.00,
-    "image": "hydroflask.jpg"},
+        "name": "32 oz Leakproof Tritan Fidus",
+        "price": 15.00,
+        "image": "fidus1.jpg"
+    },
+    {
+        "name": "Large Half Gallon/64 oz Motivational Fidus",
+        "price": 20.00,
+        "image": "fidus2.jpg"
+    },
+    {
+        "name": "1 Gallon Motivational Fidus",
+        "price": 25.00,
+        "image": "fidus3.jpg"
+    }                                                      
+]
 
+// Creates array of products for hydromate
+var hydromate = [
     {
-    "brand": "Coldest",
-    "price": 50.00,
-    "image": "coldest.jpg"}
-    ];
+        "name": "32 oz with Straw Hydromate",
+        "price": 20.00,
+        "image": "hydromate1.jpg"
+    },
+    {
+        "name": "64 oz with Straw Hydromate",
+        "price": 25.00,
+        "image": "hydromate2.jpg"
+    },
+    {
+        "name": "128 oz with Straw Hydromate",
+        "price": 30.00,
+        "image": "hydromate3.jpg"
+    }                                                            
+]
+
+// Creates array of products that contains all products
+var products =
+    {
+        "hydroflask": hydroflask,
+        "pogo": pogo,
+        "fidus": fidus, 
+        "hydromate": hydromate
+    }
+    
 if(typeof module != 'undefined') {
      module.exports.products = products;
     }
--- Wang_Vincent_Assignment3/server.js
+++ Wang_Vincent_Assignment3/server.js
@@ -12,6 +12,12 @@ var qs = require('qs'); // Starts up qs system actions
 var fs = require('fs'); // Starts up fs system actions
 const querystring = require('querystring');
 const { response } = require('express');
+var cookieParser = require('cookie-parser'); // Sets cookie-parser
+var session = require('express-session'); // Sets express-session module 
+const nodemailer = require("nodemailer"); // Sets node mailer module 
+app.use(cookieParser()); // Use cookie-parser
+
+app.use(session({secret: "ITM352 rocks!"})); // Taken from Lab 15
 
 // Reads user_data.json - Lab 14 code
 var user_data_file = "./user_data.json";
@@ -26,33 +32,169 @@ app.all('*', function (request, response, next) {
     next();
 });
 
-app.post("/process_page", function (request, response, next) {
-    // following code same as code in products_display but modified
-    let POST = request.body;
-    if (typeof POST['purchase_submit'] != 'undefined') {
-    const stringified = querystring.stringify(POST);
-    has_errors = false; // assume quantities are valid
-    total_qty = 0; 
-    for (i in products) {
-        if (typeof POST[`quantity${i}`] != "undefined") {
-            a_qty = POST[`quantity${i}`];
-            // Sticky textboxes
-            total_qty += a_qty; 
-            if(!isNonNegInt(a_qty)) {
-                has_errors = true; //invalid quantity error
-            }
+// Generates invoice based off the one in our cart.html and emails it to the users email. 
+// Nodemailer Code referenced from Daniel Port's Assingment 3 example code
+app.post("/checkout", function (request, response) {
+
+        // Generate HTML invoice string
+        var user_email = user_data[request.cookies.username].email; // sets email from user_data file to user_email
+        var name = user_data[request.cookies.username].fullname // sets fullname from user_data file to name
+        cart = JSON.parse(request.query['cartData']); // parses for cartData and puts in cart
+
+        // following invoice taken from cart.html
+      var invoice_str = `<link rel="stylesheet" href="invoice.css">
+      <title>Invoice</title>
+      </head>
+      <body>
+      <table border="2">
+          <tbody>
+              <script>
+            <tr>
+              <th style="text-align: center;" width="43%">Item</th>
+              <th style="text-align: center;" width="11%">Quantity</th>
+              <th style="text-align: center;" width="13%">Price</th>
+              <th style="text-align: center;" width="54%">Extended Price</th>
+            </tr>`
+            
+      
+            // Code taken from Invoice4 and modified while referring to example code from Assignment1 Instructions
+            // Displays Purchased items
+            subtotal = 0;
+            for (product in products) {
+                for (i = 0; i < products[product].length; i++) {
+                  quantity = cart[`${product}${i}`]
+                  if (quantity > 0) {
+                    extended_price = quantity * products[product][i].price
+                    subtotal += extended_price;
+            
+            invoice_str +=
+             `
+            <tr>
+              <td width="43%">${products[product][i].name}</td>
+              <td align="center" width="11%">${quantity}</td>
+              <td width="13%">\$${products[product][i].price}</td>
+              <td width="54%">\$${extended_price}</td>
+            </tr>`
+      
+            
+      }
+      }
+      }
+      
+      // All code below taken from Invoice4 and altered
+      // Compute Sales Tax
+      var tax = 0.0575;
+      var tax_amount = subtotal * tax;
+      // Compute Shipping Fee depending on Subtotal amount
+      var shipping_fee;
+      var small_fee = 2;
+      var medium_fee = 5;
+      var large_fee = 0.05;
+      if (subtotal < 50) {
+        shipping_fee = small_fee;
+      }
+      else if (subtotal >= 50 && subtotal < 100) {
+        shipping_fee = medium_fee;
+      }
+      else if (subtotal >= 100) {
+        shipping_fee = subtotal * large_fee;
+      }
+      
+      // Compute the Grand Total
+      var grand_total = subtotal + tax_amount + shipping_fee;
+
+      invoice_str += `
+            // Displays receipt
+            <tr>
+              <td style="text-align: center;" colspan="3" width="67%">Sub-total</td>
+              <td width="54%">\$${subtotal.toFixed(2)}</td>
+            </tr>
+            <tr>
+              <td style="text-align: center;" colspan="3" width="67%">Tax @ 5.75%</span></td>
+              <td width="54%">\$${tax_amount.toFixed(2)}</td>
+            </tr>
+            <tr>
+              <td style="text-align: center;" colspan="3" width="67%">Shipping</span></td>
+              <td width="54%">\$${shipping_fee.toFixed(2)}</td>
+            </tr>
+            <tr>
+              <td style="text-align: center;" colspan="3" width="67%"><strong>Total</strong></td>
+              <td width="54%"><strong>\$${grand_total.toFixed(2)}</strong></td>
+            </tr>
+            <tr>
+              <td style="text-align: center;" colspan="3" width="67%"><strong>OUR SHIPPING POLICY IS:A subtotal $0 - $49.99 will be $2 shipping A subtotal $50 - $99.99 will be $5 shipping Subtotals over $100 will be charged 5% of the subtotal amount</strong></td>
+              <td style="text-align: center;" width="54%"><H1>Thanks For Browsing ${name}! Please Confirm Purchase Below If You Would Like To Checkout!</H1></td>
+            </tr>
+            <tr>
+              <td style="text-align: center;" colspan="3" width="67%"><input type="button" value="Back to Store!" onclick="history.back()"></td>
+              <td style="text-align: center;" width="54%"><H1><input type="button" value="Confirm Purchase!" onclick="purchase()"></H1></td>
+            </tr>
+         
+          </script>
+          </tbody>
+        </table>      
+      </body>`
+    
+      // Set up mail server. Only will work on UH Network due to security restrictions
+      var transporter = nodemailer.createTransport({
+        host: "mail.hawaii.edu",
+        port: 25,
+        secure: false, // use TLS
+        tls: {
+          // do not fail on invalid certs
+          rejectUnauthorized: false
         }
-    }
+      });
+    
+      var mailOptions = {
+        from: 'vincentwang12345@gmail.com',
+        to: user_email,
+        subject: 'Hydration Station Invoice',
+        html: invoice_str
+      };
+    
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          invoice_str += '<br>It dont work. go away im still testing';
+        } else {
+          invoice_str += `<br>Your invoice was mailed to ${user_email}`;
+        }
+        response.send(invoice_str);
+      });
+     
+    });
+
+// Since we are now using cookies and sessions I have decided to comment out the following code as it is no longer needed
+// The following redirects to the invoice with a query string if no errors however we are no longer using query strings
+// We also are no longer redirecting to different pages - navigation is now through the nav bars
+
+//app.post("/process_page", function (request, response, next) {
+    // following code same as code in products_display but modified
+//    let POST = request.body;
+//    if (typeof POST['purchase_submit'] != 'undefined') {
+//    const stringified = querystring.stringify(POST);
+//    has_errors = false; // assume quantities are valid
+//    total_qty = 0; 
+//    for (i in products) {
+//        if (typeof POST[`quantity${i}`] != "undefined") {
+//            a_qty = POST[`quantity${i}`];
+            // Sticky textboxes
+//            total_qty += a_qty; 
+//            if(!isNonNegInt(a_qty)) {
+//                has_errors = true; //invalid quantity error
+//            }
+//        }
+//    }
     // Respond to errors or redirect to login if no errors - response.redirect code found from Getting start with Assign. 2 screencast
-    if(has_errors) { // if has errors then keeps user on products_display page
-        response.redirect("./products_display.html?" + stringified);
-    } else if(total_qty == 0) { // if no quantity selection then keeps user on products_display page
-        response.redirect("./products_display.html?" + stringified);
-    } else { // if nothing wrong then send user to login page
-        response.redirect("./login.html?" + stringified);
-    }
-}
-})
+//    if(has_errors) { // if has errors then keeps user on products_display page
+        //response.redirect("./index.html?" + stringified);
+//    } else if(total_qty == 0) { // if no quantity selection then keeps user on products_display page
+        //response.redirect("./index.html?" + stringified);
+//    } else { // if nothing wrong then send user to login page
+
+//    }
+//}
+//})
 
 // Processes login form - Modified Code from Prof Port's Assignment 2 screencast
 app.post('/process_login', function (request, response, next) {
@@ -64,8 +206,9 @@ app.post('/process_login', function (request, response, next) {
     if (typeof user_data[username_entered] != "undefined") {
         // Checks if password matches the password that is connected to that username
         if (user_data[username_entered]["password"] == password_entered) {
-            // sends user to invoice if no problems
-        response.redirect("invoice.html?" + qs.stringify(request.query));
+            // sends user back to home but now they have a cookie we can use to check if they have logged in
+        response.cookie("username", request.body["uname"], path="/") // Gives a global username coookie
+        response.redirect("index.html")
     } else {
 
     }
@@ -73,13 +216,14 @@ app.post('/process_login', function (request, response, next) {
 
 }
 // sends back to login if login info does not match user_data info
-response.redirect("login.html?" + qs.stringify(request.query));
+response.redirect("login.html");
 })
 
 
 // Processes register form - Modified Code from Prof Port's Assignment 2 screencast
 app.post('/process_register', function (request, response, next) {
     request.query["uname"] = request.body["uname"];  
+    request.query["fullname"] = request.body["fullname"]; 
     request.query["email"] = request.body["email"]; 
     request.query["psw"] = request.body["psw"]; 
     request.query["pswRepeat"] = request.body["pswRepeat"];
@@ -93,21 +237,26 @@ app.post('/process_register', function (request, response, next) {
     if (unameRegex.test(request.body.uname)) {
 
     } else {
-        response.redirect("register.html?" + qs.stringify(request.query));
+        response.redirect("register.html");
     }
     // Sets uname to min of 4 char and max of 10 char
     if ((request.body.uname.length < 4) || (request.body.uname.length > 10)) {
-        response.redirect("register.html?" + qs.stringify(request.query));
+        response.redirect("register.html");
+    }
+
+    // Sets fullname to max of 30 char
+    if (request.body.fullname.length > 30) {
+        response.redirect("register.html");
     }
 
     // PASSWORD VALIDATION:
     // Sets psw to min of 6 char
     if (request.body.psw.length < 6) {
-        response.redirect("register.html?" + qs.stringify(request.query));
+        response.redirect("register.html");
     }
     // Makes sure that both psw and pswRepeat are the same
     if (request.body.psw != request.body.pswRepeat){
-        response.redirect("register.html?" + qs.stringify(request.query));
+        response.redirect("register.html");
     }
 
     // EMAIL VALIDATION:
@@ -115,7 +264,7 @@ app.post('/process_register', function (request, response, next) {
     if (emailRegex.test(request.body.email)) {
 
     } else {
-        response.redirect("register.html?" + qs.stringify(request.query));
+        response.redirect("register.html");
     }
 
     
@@ -124,22 +273,18 @@ app.post('/process_register', function (request, response, next) {
     // Adds uname to user_data in lowercase to make uname case insensitive
     username = request.body["uname"].toLowerCase(); 
     user_data[username] = {};
+    user_data[username].fullname = request.body["fullname"];
     user_data[username].password = request.body["psw"];
     user_data[username].email = request.body["email"];
     // save updated user_data to file (DB)
     fs.writeFileSync(user_data_file, JSON.stringify(user_data));
-    response.redirect("invoice.html?" + qs.stringify(request.query)); 
+    // Give global cookies for username, fullname, and email
+    response.cookie("username", request.body["uname"].toLowerCase(), path="/");
+    response.cookie("fullname", request.body["fullname"], path="/");
+    response.cookie("email", request.body["email"], path="/");
+    response.redirect("index.html"); 
 });
 
 app.use(express.static('./public')); // Runs our public folder on the server
 app.listen(8080, () => console.log('listening on wang 8080')); // Runs server on 8080
 
-// function code from Invoice4
-function isNonNegInt(q, returnErrors = false) {
-    var errors = []; // assume that there are no errors
-    if(q == "") q = 0; // Considers blank inputs as 0
-    if(Number(q) != q) errors.push('Not a number!'); // Check if string is number
-    else if(q < 0) errors.push('Negative value!'); // Check if non-negative
-    else if(parseInt(q) != q) errors.push('Not an integer!'); // Check if integer
-    return returnErrors ? errors : (errors.length == 0);
-    }
